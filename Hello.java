62b3cc6c8502ae4e2b75b0849fa31744bf3ce1d3
vinod <vinod.tyagi222@gmail.com>
2019-10-07 20:44:47 +0530

    commit

---
+++ Hello.java
@@ -0,0 +1,43 @@
+class A
+{
+  static public void main(String...args)
+   {
+        System.out.println("Hello World1");
+        main();
+
+    }
+   static public void main()
+   {
+     System.out.println("main");
+    } 
+
+
+}
+class C
+{
+  static public void main(String...args)
+   {
+        System.out.println("Hello World2");
+
+    }
+
+
+}
+class B
+{
+  static public void main(String...args)
+   {
+        System.out.println("Hello World3");
+
+    }
+
+
+}
+
+//String args[] :-  param string array for command line input 
+
+//System:- it is the predefine class of java.lang package
+
+//out:-  reference variable of PrintStream Class
+
+//println():-  predefine method of PrintStream class
